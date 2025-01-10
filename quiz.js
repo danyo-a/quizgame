c1d4d55b60e93e2653e4086afa16625f358e0a39
danyo <gamebeplay@gmail.com>
2025-01-10 12:15:22 +0200

    finshed the project

--- quiz.js
+++ quiz.js
@@ -20,6 +20,11 @@ const quizList = [
     options:['Oxygen','Osmium','Ozone','Oganesson'],
     ans:'Oxygen'
   },
+  {
+    question:'1+1',
+    options:['2','3','10','11'],
+    ans:'2'
+  },
 ];
 const submitButton = document.querySelector('.submit-button');
 const quiz = document.querySelector('.quiz');
@@ -33,39 +38,74 @@ function startquiz (){
   showquiz();
 }
 function showquiz(){
-  let curntqiz = quizList[curentquizindex]
-    const header = document.createElement("h3");
+    let curntqiz = quizList[curentquizindex]
     trueans = curntqiz.ans;
+    const header = document.createElement("h3");
     header.innerHTML=curntqiz.question;
     quiz.appendChild(header);
-    curntqiz.options.forEach(options=>{
+    curntqiz.options.forEach((option)=>{
       const ansbutton = document.createElement("button");
     ansbutton.classList.add("option");
     ansbutton.classList.add("option-js");
-    ansbutton.innerHTML= options;
+    ansbutton.innerHTML= option;
     quiz.appendChild(ansbutton);
-    trueans = curntqiz.ans;
-    ansbutton.addEventListener('click',()=>{
-      document.querySelector('.submit-button').style.display="block";
-      if(ansbutton.textContent===trueans){
-        ansbutton.style.backgroundColor="lightgreen";
-        document.querySelectorAll('.option-js').forEach((button)=>{button.disabled=true;});
-      }
-      else{
-        ansbutton.style.backgroundColor="rgb(212,46,46)";
-        document.querySelectorAll('.option-js').forEach((button)=>{
-          if(button.textContent===trueans){
-            button.style.backgroundColor="lightgreen";
-          }
-
-          button.disabled=true;
-        });
-      }
     });
-  })
+      let button = document.querySelectorAll('.option-js');
+      button.forEach((ansbutton)=>{
+        ansbutton.addEventListener('click',()=>{
+          submitButton.style.display="block";
+          if(ansbutton.textContent===trueans){
+            ansbutton.style.backgroundColor="lightgreen";
+            score++;
+            document.querySelectorAll('.option-js').forEach((button)=>{button.disabled=true;});
+          }
+          else{
+            ansbutton.style.backgroundColor="rgb(212,46,46)";
+            document.querySelectorAll('.option-js').forEach((button)=>{
+              if(button.textContent===trueans){
+                button.style.backgroundColor="lightgreen";
+              }
+
+              button.disabled=true;
+            });
+          }
+        });
+
+      })
+    
+  
     
   curntqiz=[];
   
 }
 startquiz();
+submitButton.addEventListener('click',()=>{nextQuiz()});
+function showscore(){
+  quiz.innerHTML=`<h3> you get ${score} of ${quizList.length}`
+  submitButton.innerHTML="play agin";
+  submitButton.style.display="block";
+}
+function handel (){
+  curentquizindex++;
+  if(curentquizindex<quizList.length){
+    showquiz();
+  }
+  else{
+    showscore();
+  }
+}
 
+function nextQuiz(){
+  submitButton.style.display="none";
+  quiz.innerHTML="";
+  if(curentquizindex<quizList.length){
+    handel();
+  }
+  else{
+    startquiz();
+  }
+  console.log(curentquizindex);
+}
+function playagin(){
+  
+}
